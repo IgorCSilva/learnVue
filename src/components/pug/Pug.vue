fb15502910799e16e5ce701ee6ae382ee86c7d25
Igor Carneiro <ics@cin.ufpe.br>
2018-11-14 14:05:16 -0300

    Estudos com pug

--- src/components/pug/Pug.vue
+++ src/components/pug/Pug.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
-    
+
+    //- Incluindo mixin
+    include includes/mixinComment
+
     .pug 
 
-        h1 PUG 
+        h1 PUG
 
         p Attributes
 
@@ -25,9 +28,97 @@
 
         hr 
 
-        span Boolean attributes
+        h4 Boolean attributes
         br
         input(type="text" placeholder="Campo habilitado ou não" disabled=true)
+
+        hr 
+
+        
+        h3 For/Each loop
+
+        each n in [1, 2, 3, 4]
+            p= n
+        
+        h4 Using index
+        each n, i in [1, 20, 30, 4]
+            p= i + ' - ' + n
+        
+        h4 Using array of names
+        - const names = ['Dom', 'Tom', 'Malorie']
+        each n, i in names
+            p= i + ' - ' + n
+
+        h4 Using json object
+        - const object = { name: 'Igor', age: '25'}
+        each n, i in object
+            p= i + ' - ' + n
+
+        h4 Using for/else 
+        - const emptyArray = []
+        for n, i in emptyArray        
+            p= i + ' - ' + n
+        else 
+            br
+            strong Empty Array!
+        
+        hr 
+
+        h3 If/Else Statements
+        
+        - let user = {name: 'Domenic', loggedin: false, lastLogin: 6}
+
+        if (user.loggedin)
+            p 
+                | Welcome back 
+                strong #{ user.name }
+        else if (user.lastLogin < 10)
+            p Your lasy login was #{ user.lastLogin } minutes ago.
+            p 
+                a(href="/login") Log in Again
+        else 
+            a(href="/login") Log in
+
+        hr 
+
+        h3 Case Statement
+
+        - const orderStatus = 'Completed'
+
+        case orderStatus
+            when ('Pending')
+                p Your order has been placed and will be sent shortly.
+            
+            when ('In_Transit')
+                p Your order is on the move.
+            
+            when ('Completed')
+                p Your order has been completed.
+            
+            default
+                p Sorry! The option does not exist.
+             
+        
+        hr 
+
+        h3 Include Statement
+        
+        //- Incluindo arquivo externo.
+        include includes/includeFile
+
+        hr 
+
+        h3 Mixins
+        
+        - const c = {postedByAdmin: true, date: '01-02-04', author: 'Igor', text: 'Learning Pug'}
+
+        +comment(c)
+
+        - const commentd = {date: '51-02-04', author: 'Carneiro', text: 'Another mixin call!'}
+
+        +comment(commentd)
+
+
 
 
 
